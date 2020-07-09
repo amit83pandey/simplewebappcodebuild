b690ce3d3469a6df1a86ac39a13868f539ee9f72
amit83pandey <amit83pandey@gmail.com>
2020-07-09 12:49:21 +0530

    Add files via upload

--- src/main/java/com/simpletest/App.java
+++ src/main/java/com/simpletest/App.java
@@ -1,1 +1,14 @@
+package com.simpletest;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+         System.out.println( "Hello World 1111!" );
+    }
+}
