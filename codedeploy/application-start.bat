4d829333ce487ca9c38a8bc80bd36ce7f48c70c5
amit83pandey <amit83pandey@gmail.com>
2020-07-14 18:20:47 +0530

    Update application-start.bat

--- codedeploy/application-start.bat
+++ codedeploy/application-start.bat
@@ -1,3 +1,3 @@
 set CATALINA_HOME=C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57
 SET JRE_HOME=C:\Users\Administrator\Desktop\codedeploy\amazon-corretto-8.252.09.2-windows-x64-jre\jre8\
-start C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57\bin\startup.bat > NUL
+start C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57\bin\startup.bat
