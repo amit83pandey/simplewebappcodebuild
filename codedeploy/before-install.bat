f410099308f0ce5863f5f13c0d3c29de3d7ff63a
amit83pandey <amit83pandey@gmail.com>
2020-07-14 19:31:02 +0530

    Update before-install.bat

--- codedeploy/before-install.bat
+++ codedeploy/before-install.bat
@@ -1,3 +1,1 @@
-set CATALINA_HOME=C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57
-SET JRE_HOME=C:\Users\Administrator\Desktop\codedeploy\amazon-corretto-8.252.09.2-windows-x64-jre\jre8\
-C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57\bin\shutdown.bat
+
