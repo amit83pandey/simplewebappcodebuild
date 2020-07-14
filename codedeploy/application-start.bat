1d37624cca8483a3d11d75ad7c9c75078062e5a4
amit83pandey <amit83pandey@gmail.com>
2020-07-14 19:30:25 +0530

    Update application-start.bat

--- codedeploy/application-start.bat
+++ codedeploy/application-start.bat
@@ -1,3 +1,1 @@
-set CATALINA_HOME=C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57
-SET JRE_HOME=C:\Users\Administrator\Desktop\codedeploy\amazon-corretto-8.252.09.2-windows-x64-jre\jre8\
-start C:\Users\Administrator\Downloads\apache-tomcat-8.5.57-windows-x64\apache-tomcat-8.5.57\bin\startup.bat
+start C:\BeatBlip\BeatBlipConsole\beatblip\beatblip.exe  -open "C:\BeatBlip\console.txt"
