febde5575be65dd4440d93ab28d22c010dff9548
amit83pandey <amit83pandey@gmail.com>
2020-07-16 12:47:24 +0530

    Update application-start.bat

--- codedeploy/application-start.bat
+++ codedeploy/application-start.bat
@@ -1,3 +1,1 @@
-for /F "tokens=*" /F %%j in (C:\Windows\System32\config\systemprofile\BeatBlip\settings\status.txt) do set statusvalue=%%j
-echo %statusvalue%
-if %statusvalue%==1 (echo "fail")  else (echo "pass")
+C:\BeatBlip\test.bat
